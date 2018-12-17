4501d61427d6f05b69725672071924078e0efd6f
Jeancy Tshibemba Mukendi <jeancyts@gmail.com>
2018-12-17 21:10:53 +0200

    First version of the body mass index (BMI) application
    This is the first version console application that prompt a user to enter their height and weight then tell them if their underweight if BMI < 18.5 , normal if the 18.5 <= BMI and bmi < 25 overweight if 25 <= BMI and BMI < 30 then obese if the BMI is greater or equal 30 .

---
+++ BodyMassIndex.java
@@ -0,0 +1,38 @@
+/**
+*Project: Computing Body Mass Index
+*weekend project.
+*
+*/
+package bodymassindex;
+/**
+* @ Author Jeancy Tshibemba
+* @ version 1.0
+*/
+
+public class BodyMassIndex{
+    
+    public static void compute(){
+		Scanner scanner = new Scanner(System.in);
+	    System.out.println("Enter the weight in pounds please...>>");
+		double we = scanner.nextDouble();// weight in pounds
+		double weight = we * 0.45359237 ;// weight in kilograms
+		System.out.println ("Enter the height in inches please...>>");
+		scanner.nextLine();
+		double h = scanner.nextDouble();//height in inches
+		double height = h * 0.025; // height in meters
+		double bmi = weight /( height*height); // Body Mass Index formula
+		if(bmi < 18.5){
+			System.out.println("You are underweight");
+		}else if( (bmi >= 18.5) && (bmi < 25)){
+			System.out.println("Your weight is normal")
+		}else if((bmi >= 25)&&( bmi < 30)){
+			System.out.println("You are overweight");
+		}else{
+			System.out.println("You are obese");
+		}
+	}
+	public static void main(String [] args){
+		BodyMassIndex.compute();// Calling the compute method general context
+		compute(); //This an alternative way to call a static method in the same class.
+	}
+}
